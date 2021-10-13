8552775d6367d93788ce770e694420e9dd006e67
Кристина <kristina.ezikova@yandex.ru>
2021-10-13 21:43:04 +0300

    Add service usage

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"log"
 	"notes/pkg/app"
+	"notes/pkg/config"
+	"notes/pkg/db"
+	"notes/pkg/repo"
+	"notes/pkg/service"
 )
 
 func main() {
-	app := app.New()
+	cfg := config.New()
+	db_, err := db.NewDB(cfg.DbDsn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	adapters := db.New(db_)
+	repo := repo.New(adapters)
+	service := service.New(repo)
+
+	app := app.New(service)
 	app.Run()
 }
