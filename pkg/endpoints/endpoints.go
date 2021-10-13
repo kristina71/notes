c969f4aa0808336c79144ee6d269451e23ea9fb3
Кристина <kristina.ezikova@yandex.ru>
2021-10-13 21:37:09 +0300

    Impement getNotes endpoint

--- pkg/endpoints/endpoints.go
+++ pkg/endpoints/endpoints.go
@@ -2,16 +2,66 @@ package endpoints
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"notes/pkg/models"
+	"notes/pkg/service"
+	"text/template"
+
+	_ "embed"
 
 	"github.com/gorilla/mux"
 )
 
-func New() http.Handler {
+func New(service *service.Service) http.Handler {
 	r := mux.NewRouter()
+
+	e := endpoint{service: service}
 
 	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello")
 	}).Methods(http.MethodGet)
+	r.HandleFunc("/notes", e.GetNotes).Methods(http.MethodGet)
 	return r
+}
+
+type endpoint struct {
+	service *service.Service
+}
+
+//go:embed static/notes.html
+var notesTpl string
+
+func (e endpoint) GetNotes(w http.ResponseWriter, r *http.Request) {
+	notes, err := e.service.GetNotes(r.Context())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	t, err := template.New("webpage").Parse(notesTpl)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	data := struct {
+		Title string
+		Items []models.Note
+	}{
+		Title: "My page",
+		Items: notes,
+	}
+
+	err = t.Execute(w, data)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
 }
