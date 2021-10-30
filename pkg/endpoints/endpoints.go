6b74576e6e05768b273542b67d8f67bd65c9ee49
Кристина <kristina.ezikova@yandex.ru>
2021-10-30 21:24:35 +0300

    Implement getNote endpoint

--- pkg/endpoints/endpoints.go
+++ pkg/endpoints/endpoints.go
@@ -1,12 +1,14 @@
 package endpoints
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"notes/pkg/models"
 	"notes/pkg/service"
 	"notes/pkg/tpl"
+	"strconv"
 
 	_ "embed"
 
@@ -22,6 +24,7 @@ func New(service *service.Service) http.Handler {
 		fmt.Fprintf(w, "hello")
 	}).Methods(http.MethodGet)
 	r.HandleFunc("/notes", e.GetNotes).Methods(http.MethodGet)
+	r.HandleFunc("/note/{id:.*}", e.GetNote).Methods(http.MethodGet)
 	return r
 }
 
@@ -47,4 +50,40 @@ func (e endpoint) GetNotes(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tpl.Render(w, r, data, "notesHtml")
+}
+
+func (e endpoint) GetNote(w http.ResponseWriter, r *http.Request) {
+	url := models.Note{}
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var err error
+	url.Id, err = strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	note, err := e.service.GetNote(r.Context(), url)
+
+	if err != nil {
+		log.Println(err)
+		if errors.Is(err, models.ErrNoteNotFound) {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data := struct {
+		Title string
+		Item  models.Note
+	}{
+		Title: "My note",
+		Item:  note,
+	}
+
+	tpl.Render(w, r, data, "noteHtml")
 }
